f64b9237e723135b2f3dcf30de4faebfe12c2d6c
fchanteau <francois.chanteau49@gmail.com>
2023-07-10 22:28:17 +0200

    feat(app): loading message in ProtectedRoute

--- app/src/pages/router/ProtectedRoute.tsx
+++ app/src/pages/router/ProtectedRoute.tsx
@@ -9,7 +9,9 @@ export type ProtectedRouteProps = {
 };
 
 export function ProtectedRoute(props: PropsWithChildren<ProtectedRouteProps>) {
-  const { data: user } = useCurrentUserQuery();
+  const { data: user, isLoading } = useCurrentUserQuery();
+
+  if (isLoading) return <h1>Chargement...</h1>;
 
   if (!user || user?.role < props.minRole) {
     return <Navigate to={props.redirect ?? '/'} replace />;
